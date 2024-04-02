85804e3fa851a5b01bdef73936eb33118c340180
Feveion <142549572+Feveion@users.noreply.github.com>
2024-04-01 19:44:38 -0700

    Add files via upload
    - Added a todo so you start with one
    - Separated custom functions from main.py

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-def get_todos(filepath="todos.txt"):
-    """ Read a text file and return the list of
-     to-do items"""
-    with open(filepath,'r') as file:
-        todos_local = file.readlines()
-    return todos_local
-
-def write_todos(todos_arg, filepath="todos.txt"):
-    """ Write the to-do items list in the text file."""
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
-
-
+#from functions import get_todos, write_todos
+import functions
 
 while True:
     #Get user input and strip space chars from it.
@@ -21,15 +10,15 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         todos.append(todo + '\n')
 
-        write_todos(todos, "todos.txt")
+        functions.write_todos(todos, "todos.txt")
 
     elif user_action.startswith("show"):
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -43,12 +32,12 @@ while True:
 
             number = number - 1
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             new_todo = input("enter the new todo")
             todos[number] = new_todo + '\n'
 
-            write_todos(todos, "todos.txt")
+            functions.write_todos(todos, "todos.txt")
 
         except ValueError:
             print("Your command is not valid")
@@ -58,12 +47,12 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = functions.get_todos()
             index = number - 1
             todo_to_remove = todos[index].strip('\n') #Strip it so it doesn't Indent on print message
             todos.pop(index)
 
-            write_todos(todos, "todos.txt")
+            functions.write_todos(todos, "todos.txt")
 
             message = f"Todo {todo_to_remove} was removed from the list"
             print(message)
