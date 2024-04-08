38ef46876f224e3c353c4629cd159ab9ff69c2ca
Feveion <Feveionisexploring@gmail.com>
2024-04-08 14:25:58 -0700

    - (GUI) Implemented the Complete & Exit Case

--- gui.py
+++ gui.py
@@ -7,9 +7,12 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+exit_button = sg.Button("Exit")
+complete_button = sg.Button("Complete")
 
-
-layout = [[label], [input_box, add_button],[list_box, edit_button]]
+layout = [[label], [input_box, add_button],
+          [list_box, edit_button, complete_button],
+          [exit_button]]
 
 window = sg.Window('My To-Do App',
                    layout=layout,
@@ -26,6 +29,7 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
 
         case "Edit":
             todo_to_edit = values['todos'][0]
@@ -36,7 +40,18 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(values=todos)
 
-        case 'todos':
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+
+        case "Exit":
+            break
+
+        case "todos":
             window['todo'].update(value=values['todos'])
 
         case sg.WIN_CLOSED:
